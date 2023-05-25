ba006bb6e34f649cba58dcc78f9884a1bb914157
ashustikov@kameleoon.com <ashustikov@kameleoon.com>
2023-05-25 15:30:21 -0400

    #1

---
+++ fraud/src/main/java/ca/acorn/fraud/FraudCheckController.java
@@ -0,0 +1,24 @@
+package ca.acorn.fraud;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/fraud")
+public class FraudCheckController {
+
+    private final FraudCheckService fraudCheckService;
+
+    public FraudCheckController(FraudCheckService fraudCheckService) {
+        this.fraudCheckService = fraudCheckService;
+    }
+
+    @GetMapping(path = "{customerId}")
+    public FraudCheckResponse isFraudster(
+            @PathVariable ("customerId") Integer customerId){
+        boolean isFraudulentCustomer = fraudCheckService.isFraudulent(customerId);
+        return new FraudCheckResponse(isFraudulentCustomer);
+    }
+}
---
+++ fraud/src/main/java/ca/acorn/fraud/FraudCheckHistoryRepository.java
@@ -0,0 +1,6 @@
+package ca.acorn.fraud;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory, Integer> {
+}
