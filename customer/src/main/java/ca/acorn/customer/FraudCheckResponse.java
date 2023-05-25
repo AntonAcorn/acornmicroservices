ff37791023d9c988bff6bf523e2999e876869b45
ashustikov@kameleoon.com <ashustikov@kameleoon.com>
2023-05-25 16:28:32 -0400

    #1

---
+++ customer/src/main/java/ca/acorn/customer/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package ca.acorn.customer;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
--- fraud/src/main/java/ca/acorn/fraud/FraudCheckController.java
+++ fraud/src/main/java/ca/acorn/fraud/FraudCheckController.java
@@ -1,14 +1,16 @@
 package ca.acorn.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/fraud")
+@RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudCheckController {
 
     private final FraudCheckService fraudCheckService;
@@ -17,6 +19,7 @@ public class FraudCheckController {
     public FraudCheckResponse isFraudster(
             @PathVariable ("customerId") Integer customerId){
         boolean isFraudulentCustomer = fraudCheckService.isFraudulent(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
