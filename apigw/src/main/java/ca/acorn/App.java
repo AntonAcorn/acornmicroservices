4a16735ac9422784a8d6d39f60c1a878601b7946
ashustikov@kameleoon.com <ashustikov@kameleoon.com>
2023-05-27 14:26:54 -0400

    #2 API GateWay

---
+++ apigw/src/main/java/ca/acorn/App.java
@@ -0,0 +1,13 @@
+package ca.acorn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
