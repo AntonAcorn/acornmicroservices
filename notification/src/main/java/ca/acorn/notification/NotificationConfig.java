9508ee881c8fdfd3201e5838b6300a5e24622128
ashustikov@kameleoon.com <ashustikov@kameleoon.com>
2023-05-27 17:12:45 -0400

    #2 RabbitMq

---
+++ notification/src/main/java/ca/acorn/notification/NotificationConfig.java
@@ -0,0 +1,18 @@
+package ca.acorn.notification;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NotificationConfig {
+
+    @Value("${rabbitmq.exchanges.internal}")
+    private String internalExchange;
+
+    @Value("${rabbitmq.queues.notification}")
+    private String notificationQueue;
+
+    @Value("${rabbitmq.routing-keys.internal-notification}")
+    private String internalNotificationRoutingKey;
+
+}
